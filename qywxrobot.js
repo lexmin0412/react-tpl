0b0515329b4a1e785348f812c8e20c65463f092f
lexmin0412 <zhangle_media@hotmail.com>
2021-01-06 11:50:34 +0800

    fix: 修复企业微信机器人项目名称错误

--- qywxrobot.js
+++ qywxrobot.js
@@ -14,10 +14,10 @@ const request = require( 'request' );
 var resData = {
   "msgtype": "markdown",
   "markdown": {
-    "content": `**taro-x-ui** 文档部署成功
-> 点此查看：[https://taro-xui.lexmin0412.vercel.app](https://taro-xui.lexmin0412.vercel.app)
-> 本次构建地址：[https://github.com/lexmin0412/taro-xui/actions/](https://github.com/lexmin0412/taro-xui/actions/)
-> 项目地址：[https://github.com/lexmin0412/taro-xui](https://github.com/lexmin0412/taro-xui)
+    "content": `**react-tpl** 项目部署成功
+> 点此查看：[https://react-tpl.lexmin0412.vercel.app](https://react-tpl.lexmin0412.vercel.app)
+> 本次构建地址：[https://github.com/lexmin0412/react-tpl/actions/](https://github.com/lexmin0412/react-tpl/actions/)
+> 项目地址：[https://github.com/lexmin0412/react-tpl](https://github.com/lexmin0412/react-tpl)
             `,
     "mentioned_mobile_list": [ "@all" ]
   }
